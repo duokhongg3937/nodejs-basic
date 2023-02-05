92124927a82d61136297ac5526de38c5d05ef89b
duokhongg3937 <duokhongg3937@gmail.com>
2023-02-05 11:18:32 +0700

    basic routing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
+const route = require('./routes')
 
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
 
 // HTTP Logger
 app.use(morgan('combined'))
@@ -19,14 +26,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Routes Init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
